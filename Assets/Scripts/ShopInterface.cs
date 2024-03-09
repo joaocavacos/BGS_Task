71c137041bd5ef8d669c206d2cf4c09f0e2d2004
João Cavacos <cavacosdoesgaming@gmail.com>
2024-03-09 17:01:15 +0000

    Sell Items Option & Functionality + Basic shop UI for buying, selling and leaving

--- Assets/Scripts/ShopInterface.cs
+++ Assets/Scripts/ShopInterface.cs
@@ -8,30 +8,51 @@ using UnityEngine.UI;
 public class ShopInterface : MonoBehaviour
 {
     private Transform container;
-    private Transform itemTemplate;
+    [SerializeField] private Transform itemBuyTemplate;
+    [SerializeField] private Transform itemSellTemplate;
 
-    public List<ItemSO> itemsToBuy;
+    [SerializeField] private List<ItemSO> stockItemsToBuy;
+    [SerializeField] private Inventory playerInventory; 
 
     private void Awake()
     {
         container = transform.Find("Container");
-        itemTemplate = container.Find("ShopItemTemplate");
-        itemTemplate.gameObject.SetActive(false);
     }
 
     private void Start()
     {
-        for (int i = 0; i < itemsToBuy.Count; i++)
-        {
-            CreateBuyItemButton(itemsToBuy[i], i);
-        }
-        
         Hide();
     }
 
-    private void CreateBuyItemButton(ItemSO itemData, int posIndex)
+    public void CreateBuyItems() //Creates all of the items that are on stock to buy
     {
-        Transform itemTransform = Instantiate(itemTemplate, container);
+        for (int i = 0; i < stockItemsToBuy.Count; i++)
+        {
+            CreateBuyItemButton(stockItemsToBuy[i], i);
+        }
+    }
+
+    public void CreateSellItems() //Creates all of the items that are sellable on the player's inventory
+    {
+        List<InventoryItem> inventory = playerInventory.inventory;
+        
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            CreateSellItemButton(inventory[i].itemSO, inventory[i].quantity, i);
+        }
+    }
+
+    public void DestroyItemButtons() //Destroys buy/sell buttons on shop close
+    {
+        foreach (Transform child in container)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void CreateBuyItemButton(ItemSO itemData, int posIndex) //All the necessary stuff to create a buy button
+    {
+        Transform itemTransform = Instantiate(itemBuyTemplate, container);
         itemTransform.gameObject.SetActive(true);
         RectTransform itemRectTransform = itemTransform.GetComponent<RectTransform>();
 
@@ -44,10 +65,27 @@ public class ShopInterface : MonoBehaviour
         
         var itemBtn = itemTransform.GetComponent<Button>();
         itemBtn.onClick.AddListener(() => TryBuyItem(itemData));
-
     }
 
-    private void TryBuyItem(ItemSO itemData)
+    private void CreateSellItemButton(ItemSO itemData, int quantity, int posIndex) //All the necessary stuff to create a sell button
+    {
+        Transform itemTransform = Instantiate(itemSellTemplate, container);
+        itemTransform.gameObject.SetActive(true);
+        RectTransform itemRectTransform = itemTransform.GetComponent<RectTransform>();
+
+        var shopItemHeight = 150f;
+        itemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * posIndex);
+        
+        itemTransform.Find("ItemName").GetComponent<TMP_Text>().SetText(itemData.itemName);
+        itemTransform.Find("ItemPrice").GetComponent<TMP_Text>().SetText(itemData.itemSellPrice.ToString());
+        itemTransform.Find("ItemQuantity").GetComponent<TMP_Text>().SetText(quantity.ToString());
+        itemTransform.Find("ItemIcon").GetComponent<Image>().sprite = itemData.itemIcon;
+        
+        var itemBtn = itemTransform.GetComponent<Button>();
+        itemBtn.onClick.AddListener(() => TrySellItem(itemData));
+    }
+
+    private void TryBuyItem(ItemSO itemData) //Checks for gold and buys if it's possible
     {
         if (CurrencyManager.Instance.TrySpendGold(itemData.itemBuyPrice))
         {
@@ -57,6 +95,18 @@ public class ShopInterface : MonoBehaviour
         {
             print("Can't afford item");
         }
+    }
+
+    private void TrySellItem(ItemSO itemData) //Sells and updates quantity to check if can be sold
+    {
+        CurrencyManager.Instance.SellItem(itemData);
+        UpdateSellItems();
+    }
+
+    private void UpdateSellItems()
+    {
+        DestroyItemButtons();
+        CreateSellItems();
     }
 
     public void Show()
